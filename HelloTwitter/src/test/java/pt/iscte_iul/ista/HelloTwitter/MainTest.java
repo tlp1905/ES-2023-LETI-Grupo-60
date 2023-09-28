de6b4fad1d6853c753b77765f3e595e4df59264d
tlp1905 <145682351+tlp1905@users.noreply.github.com>
2023-09-28 18:55:55 +0100

    1

---
+++ HelloTwitter/src/test/java/pt/iscte_iul/ista/HelloTwitter/MainTest.java
@@ -0,0 +1,20 @@
+package pt.iscte_iul.ista.HelloTwitter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A simple unit test
+ */
+public class MainTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
